54d365fb62b42f128c0e37af9c1fc6da9269dcca
Giemme13 <gianmarco.debortoli@mail.polimi.it>
2021-11-12 18:45:11 +0100

    Input impedance n conical horns

--- HW3/HW3.m
+++ HW3/HW3.m
@@ -13,8 +13,9 @@ m = 4;
 L = 0.4;         %total length of the horn [m]
 fmin = 0;        %frequency study range [fmin, fmax] [Hz]
 fmax = 2000;
+freq_res = 10000;  %resolution of the frequency interval
 
-f = linspace(fmin,fmax,10000);  %frequency axis [Hz]
+f = linspace(fmin,fmax,freq_res);  %frequency axis [Hz]
 omega = 2*pi*f;         %frequency axis [rad/s]
 k = omega/c;            %wavenumber [m^-1]
 b = sqrt(k.^2-m^2);     %geometrical quantity [m^-1]
@@ -128,9 +129,63 @@ figure(10)
 plot(f,db(abs(ZinEXP)), 'linewidth', 2)
 hold on
 plot(f,db(abs(ZinCON_1)))
-plot(f,db(abs(ZinCON_n)))
+plot(f,db(abs(ZinCON_n)), '-.')
 legend()
 hold off
+
+
+%% FROM 1 TO n CONICAL HORNS
+clc
+n = 3;
+
+e1 = zeros(1, n);
+e2 = zeros(1, n);
+
+for j = 1:n
+    delta = L/j;
+    points = round(res/j);
+    
+    Zload = Zair;
+    for i = 1:j
+        a2 = a0*exp(m*(j-i+1)*delta);
+        a1 = a0*exp(m*(j-i)*delta);
+        x1 = (a1*delta)/(a2-a1);
+        x2 = x1+delta;
+        theta1 = atan(k*x1);
+        theta2 = atan(k*x2);
+        S1 = pi*(a1^2);
+        S2 = pi*(a2^2);
+        disp([a1,a2])
+        x = linspace(0, delta, points);
+        slope = (a2-a1)/delta;
+        a = slope*x + a1;
+
+        numCON = 1i*Zload.*(sin(k*delta-theta2)./sin(theta2))+(rho*c/S2)*sin(k*delta);
+        denCON = Zload.*(sin(k*delta+theta1-theta2)./(sin(theta1).*sin(theta2)))-(1i*rho*c/S2).*(sin(k*delta+theta1)./sin(theta1));
+        ZinCON_n = (rho*c/S1)*numCON./denCON;
+
+        Zload = ZinCON_n;
+
+        %figure(j)
+        %plot((j-i)*delta+x, a, 'k', 'linewidth', 2)
+        %hold on
+        %plot((j-i)*delta+x, -a, 'k', 'linewidth', 2)
+        %yline(0, '-.')
+    end
+    %hold off
+    
+    %figure(n+j)
+    %subplot(2,1,1)
+    %plot(f,db(abs(ZinCON_n)))
+    %subplot(2,1,2)
+    %plot(f, angle(ZinCON_n))
+    
+    %for k = 1:length(f)
+        %e1(1,j) = e1(1,j) + (abs(ZinCON_n(k) - ZinEXP(k)))^2;
+    %end
+    %e1(1,j) = e1(1,j)/(2*pi*(fmax-fmin));
+end
+
 
 %% n CONICAL HORNS
 
@@ -177,3 +232,5 @@ for i=1:length(n)
 end
 
 %plot(n, e1)
+
+
