594097914174c358d529d85aad91b10386b60f70
Giemme13 <gianmarco.debortoli@mail.polimi.it>
2021-11-25 17:18:27 +0100

    computation of the admittance directly on matlab

--- HW4/HW4.m
+++ HW4/HW4.m
@@ -23,17 +23,65 @@ k = m*((2*pi*fd)^2 + R^2/(4*m^2));   %equivalent stiffness coefficient of
                                      %mass-spring-damped system
 f0 = sqrt(k/m)/(2*pi);      %natural resonance frequency [Hz]
 
-%% Equivalent circuit
-
 S = pi*D^2/4;               %cross sectional area of the venting tube [m^2]
-delta_L = 0.85*D/2;         %end correction on one end of the venting pipe
+delta_L = 0.85*D/2;         %end correction on one end of the venting pipe [m]
 
-L2 = rho*(L+2*delta_L)/S;   %inductor for the mass of the venting tube
-R2 = rho*c/S;               %resistance for the dissipation inside the venting tube
-C2 = V/(rho*c^2);           %condenser for the air volume
-L1 = m;                     %inductor for the mass of the loudspeaker
-C1 = 1/k;                   %condenser for the equivalent spring of the loudspeaker
-R1 = R;                     %resistance for the damping of the loudspeaker
+%% Equivalent electrical circuit
+
+% diaphragm
+L1 = m;                     %inductor for the mass of the loudspeaker [kg]
+k1 = k;
+C1 = 1/k1;                   %condenser for the equivalent spring of the loudspeaker [s^2/kg]
+R1 = R;                     %resistance for the damping of the loudspeaker [kg/s]
+
+% cavity
+C2 = V/(rho*c^2*S^2);       %condenser for the air volume [s^2/kg]
+k2 = 1/C2;
+
+%venting tube
+L2 = rho*(L+2*delta_L)*S;   %inductor for the mass of the venting tube [kg]
+R2 = rho*c*S;               %resistance for the dissipation inside the venting tube [kg/s]
+
+
+%% Admittance computation
+
+omega = linspace(0,20000*2*pi, 100000);
+
+%diaphragm
+Zmd = 1i.*omega.*L1;
+Zrd = R1;
+Zcd = 1./(1i.*omega*C1);
+
+Zd = Zmd + Zrd + Zcd;
+
+%cavity
+Zcc = 1./(1i.*omega*C2);
+
+%venting tube
+Zmt = 1i.*omega*L2;
+Zrt = R2;
+
+Zt = Zmt + Zrt;
+
+Z = Zd + (1./(1./Zcc + 1./Zt));
+Y = 1./Z;
+
+
+figure(1)
+subplot(2,1,1)
+plot(omega/(2*pi), db(abs(Y)), 'linewidth', 2.5)
+grid on
+xlabel('Frequency [Hz]', 'fontsize', 17)
+ylabel('$|Y|\,[dB]$', 'interpreter', 'latex', 'fontsize', 17)
+title('Magnitude', 'fontsize', 20)
+subplot(2,1,2)
+plot(omega/(2*pi), angle(Y), 'linewidth', 2.5)
+grid on
+xlabel('Frequency [Hz]', 'fontsize', 17)
+ylabel('$\angle Z\,[deg]$', 'interpreter', 'latex', 'fontsize', 17)
+yticks([-pi,-pi/2,0,pi/2,pi])
+yticklabels({'-180','-90','0','90','180'})
+title('Phase', 'fontsize', 20)
 
 
 %% FRF of mechanical circuit
@@ -47,19 +95,15 @@ time_step = out.SimulationMetadata.ModelInfo.SolverInfo.FixedStepSize;
 fs = 1/time_step;
 f_mech = linspace(0, fs/2, length(FRF_mech));
 
-figure(1)
+figure(2)
 subplot(2,1,1)
-hold on
 plot(f_mech, db(abs(FRF_mech)), 'linewidth', 2.5)
-hold off
 grid on
 xlabel('Frequency [Hz]', 'fontsize', 17)
 ylabel('$|Z|\,[dB]$', 'interpreter', 'latex', 'fontsize', 17)
 title('Magnitude', 'fontsize', 20)
 subplot(2,1,2)
-hold on
 plot(f_mech, angle(FRF_mech), 'linewidth', 2.5)
-hold off
 grid on
 xlabel('Frequency [Hz]', 'fontsize', 17)
 ylabel('$\angle Z\,[deg]$', 'interpreter', 'latex', 'fontsize', 17)
@@ -78,19 +122,15 @@ time_step = out.SimulationMetadata.ModelInfo.SolverInfo.FixedStepSize;
 fs = 1/time_step;
 f_elec = linspace(0, fs/2, length(FRF_elec));
 
-figure(2)
+figure(3)
 subplot(2,1,1)
-hold on
 plot(f_elec, db(abs(FRF_elec)), 'linewidth', 2.5)
-hold off
 grid on
 xlabel('Frequency [Hz]', 'fontsize', 17)
 ylabel('$|Z|\,[dB]$', 'interpreter', 'latex', 'fontsize', 17)
 title('Magnitude', 'fontsize', 20)
 subplot(2,1,2)
-hold on
 plot(f_elec, angle(FRF_elec), 'linewidth', 2.5)
-hold off
 grid on
 xlabel('Frequency [Hz]', 'fontsize', 17)
 ylabel('$\angle Z\,[deg]$', 'interpreter', 'latex', 'fontsize', 17)
@@ -101,11 +141,11 @@ title('Phase', 'fontsize', 20)
 
 %% Comparison
 
-figure(3)
+figure(4)
 subplot(2,1,1)
 hold on
-plot(f_mech, db(abs(FRF_mech)), 'linewidth', 2.5)
-plot(f_elec, db(abs(FRF_elec)), '--', 'linewidth', 2.5)
+plot(f_mech, db(abs(FRF_mech)), 'linewidth', 2)
+plot(f_elec, db(abs(FRF_elec)), '--', 'linewidth', 2)
 hold off
 grid on
 legend('Mechanical circuit', 'Electrical circuit', 'fontsize', 20)
@@ -114,8 +154,8 @@ ylabel('$|Z|\,[dB]$', 'interpreter', 'latex', 'fontsize', 17)
 title('Magnitude', 'fontsize', 20)
 subplot(2,1,2)
 hold on
-plot(f_mech, angle(FRF_mech), 'linewidth', 2.5)
-plot(f_elec, angle(FRF_elec), '--', 'linewidth', 2.5)
+plot(f_mech, angle(FRF_mech), 'linewidth', 2)
+plot(f_elec, angle(FRF_elec), '--', 'linewidth', 2)
 hold off
 grid on
 xlabel('Frequency [Hz]', 'fontsize', 17)
