09ce089f20bbb2d464c47b7a61c331e70a3b8ed5
Giemme13 <gianmarco.debortoli@mail.polimi.it>
2021-11-28 15:15:59 +0100

    tree admittance implementation in matlab

--- HWL2/HL2.m
+++ HWL2/HL2.m
@@ -25,6 +25,34 @@ L = rho*(l_tot)/S;      %inductor for the mass of the venting tube
 R = rho*c/S;            %resistance for the venting tube
 
 
+%% Analytical admittance computation
+
+omega = linspace(0,10000*2*pi, 100000);
+Z = 1i*omega*L + R + 1./(1i*omega*C);
+Y = 1./Z;
+
+f_axis = omega/(2*pi);
+
+figure()
+subplot(2,1,1)
+plot(f_axis, db(abs(Y)), 'linewidth', 2.5)
+grid on
+xlim([0,3000])
+ylim([-140,0])
+xlabel('Frequency [Hz]', 'fontsize', 17)
+ylabel('$|Y|\,[dB]$', 'interpreter', 'latex', 'fontsize', 17)
+title('Magnitude', 'fontsize', 20)
+subplot(2,1,2)
+plot(f_axis, angle(Y), 'linewidth', 2.5)
+grid on
+xlim([0,3000])
+xlabel('Frequency [Hz]', 'fontsize', 17)
+ylabel('$\angle Y\,[deg]$', 'interpreter', 'latex', 'fontsize', 17)
+yticks([-pi,-pi/2,0,pi/2,pi])
+yticklabels({'-180','-90','0','90','180'})
+title('Phase', 'fontsize', 20)
+
+
 %% A) Transfer function (admittance) in simscape
 
 U = fft(out.U.Data);
@@ -36,19 +64,19 @@ time_step = out.SimulationMetadata.ModelInfo.SolverInfo.FixedStepSize;
 fs = 1/time_step;
 f = linspace(0, fs/2, length(FRF));
 
-figure(1)
+figure()
 subplot(2,1,1)
 plot(f, db(abs(FRF)), 'linewidth', 2.5)
 grid on
-xlim([0,2000])
-ylim([-120,0])
+xlim([0,3000])
+ylim([-140,0])
 xlabel('Frequency [Hz]', 'fontsize', 17)
 ylabel('$|Y|\,[dB]$', 'interpreter', 'latex', 'fontsize', 17)
 title('Magnitude', 'fontsize', 20)
 subplot(2,1,2)
 plot(f, angle(FRF), 'linewidth', 2.5)
 grid on
-xlim([0,2000])
+xlim([0,3000])
 xlabel('Frequency [Hz]', 'fontsize', 17)
 ylabel('$\angle Y\,[deg]$', 'interpreter', 'latex', 'fontsize', 17)
 yticks([-pi,-pi/2,0,pi/2,pi])
@@ -56,12 +84,51 @@ yticklabels({'-180','-90','0','90','180'})
 title('Phase', 'fontsize', 20)
 
 
-%% B)  Compute the natural frequency
+%% B) Compute the natural frequency
 
 %from simscape
-[pks, locs] = findpeaks(db(abs(FRF)));
-f_0s = f(locs(1));
+[pks, locs] = findpeaks(db(abs(FRF(1:3000))));
+f_0s = f(locs);
 
 %analytically
 f_0a = (c/(2*pi)) * sqrt(S/(V*l_tot));
 
+
+%% Analytical computation of the admittance of the tree
+
+K = 4;
+N = 3;
+
+Z = 1i*omega*L + R + 1./(1i*omega*C);
+Zden = zeros(1,length(Z));
+
+for k = 1:K-1
+    for n = 1:N
+        Zden = Zden + 1./Z;
+    end
+    Z = 1i*omega*L + R + 1./(1i*omega*C + Zden);
+    Zden = zeros(1,length(Z));
+end
+
+Y = 1./Z;
+
+figure()
+subplot(2,1,1)
+plot(f_axis, db(abs(Y)), 'linewidth', 2.5)
+grid on
+xlim([0,3000])
+xlabel('Frequency [Hz]', 'fontsize', 17)
+ylabel('$|Y|\,[dB]$', 'interpreter', 'latex', 'fontsize', 17)
+title('Magnitude', 'fontsize', 20)
+subplot(2,1,2)
+plot(f_axis, angle(Y), 'linewidth', 2.5)
+grid on
+xlim([0,3000])
+xlabel('Frequency [Hz]', 'fontsize', 17)
+ylabel('$\angle Y\,[deg]$', 'interpreter', 'latex', 'fontsize', 17)
+yticks([-pi,-pi/2,0,pi/2,pi])
+yticklabels({'-180','-90','0','90','180'})
+title('Phase', 'fontsize', 20)
+
+[pks,locs] = findpeaks(db(abs(Y)));
+f_res = f_axis(locs);
