d7a7a36864928439afa9fb14e9ea69bb44742b44
Giemme13 <gianmarco.debortoli@mail.polimi.it>
2021-11-28 16:12:39 +0100

    admittance computation for both article and hw

--- HWL2/HL2.m
+++ HWL2/HL2.m
@@ -94,10 +94,10 @@ f_0s = f(locs);
 f_0a = (c/(2*pi)) * sqrt(S/(V*l_tot));
 
 
-%% Analytical computation of the admittance of the tree
+%% Tree admittance computation as in the article
 
 K = 4;
-N = 3;
+N = 2;
 
 Z = 1i*omega*L + R + 1./(1i*omega*C);
 Zden = zeros(1,length(Z));
@@ -110,18 +110,18 @@ for k = 1:K-1
     Zden = zeros(1,length(Z));
 end
 
-Y = 1./Z;
+Ya = 1./Z;
 
 figure()
 subplot(2,1,1)
-plot(f_axis, db(abs(Y)), 'linewidth', 2.5)
+plot(f_axis, db(abs(Ya)), 'linewidth', 2.5)
 grid on
 xlim([0,3000])
 xlabel('Frequency [Hz]', 'fontsize', 17)
 ylabel('$|Y|\,[dB]$', 'interpreter', 'latex', 'fontsize', 17)
 title('Magnitude', 'fontsize', 20)
 subplot(2,1,2)
-plot(f_axis, angle(Y), 'linewidth', 2.5)
+plot(f_axis, angle(Ya), 'linewidth', 2.5)
 grid on
 xlim([0,3000])
 xlabel('Frequency [Hz]', 'fontsize', 17)
@@ -130,5 +130,78 @@ yticks([-pi,-pi/2,0,pi/2,pi])
 yticklabels({'-180','-90','0','90','180'})
 title('Phase', 'fontsize', 20)
 
-[pks,locs] = findpeaks(db(abs(Y)));
-f_res = f_axis(locs);
+[pks,locs] = findpeaks(db(abs(Ya)));
+f_res_a = f_axis(locs);
+
+
+%% Tree admittance computation as in the HW
+
+K = 4;
+N = 2;
+
+Z1 = 1i*omega*L + R + 1./(1i*omega*C);
+Zden = 1./Z1;
+Zprev = zeros(1,length(Z));
+
+for k = 1:K-1
+    for n = 1:N-1
+        Zden = Zden + 1./Z1;
+    end
+    Zden = Zden + Zprev;
+    Z = 1i*omega*L + R + 1./(1i*omega*C + Zden);
+    Zprev = 1./Z;
+    Zden = zeros(1,length(Z));
+end
+
+Yb = 1./Z;
+
+figure()
+subplot(2,1,1)
+plot(f_axis, db(abs(Yb)), 'linewidth', 2.5)
+grid on
+xlim([0,3000])
+xlabel('Frequency [Hz]', 'fontsize', 17)
+ylabel('$|Y|\,[dB]$', 'interpreter', 'latex', 'fontsize', 17)
+title('Magnitude', 'fontsize', 20)
+subplot(2,1,2)
+plot(f_axis, angle(Yb), 'linewidth', 2.5)
+grid on
+xlim([0,3000])
+xlabel('Frequency [Hz]', 'fontsize', 17)
+ylabel('$\angle Y\,[deg]$', 'interpreter', 'latex', 'fontsize', 17)
+yticks([-pi,-pi/2,0,pi/2,pi])
+yticklabels({'-180','-90','0','90','180'})
+title('Phase', 'fontsize', 20)
+
+[pks,locs] = findpeaks(db(abs(Yb)));
+f_res_b = f_axis(locs);
+
+
+%% comparison
+
+figure()
+subplot(2,1,1)
+hold on
+plot(f, db(abs(FRF)), 'linewidth', 2)
+plot(f_axis, db(abs(Ya)), '--', 'linewidth', 2)
+plot(f_axis, db(abs(Yb)), '--', 'linewidth', 2)
+hold off
+grid on
+legend('simscape', 'article', 'hw')
+xlim([0,3000])
+xlabel('Frequency [Hz]', 'fontsize', 17)
+ylabel('$|Y|\,[dB]$', 'interpreter', 'latex', 'fontsize', 17)
+title('Magnitude', 'fontsize', 20)
+subplot(2,1,2)
+hold on
+plot(f, angle(FRF), 'linewidth', 2)
+plot(f_axis, angle(Ya), '--', 'linewidth', 2)
+plot(f_axis, angle(Yb), '--', 'linewidth', 2)
+hold off
+grid on
+xlim([0,3000])
+xlabel('Frequency [Hz]', 'fontsize', 17)
+ylabel('$\angle Y\,[deg]$', 'interpreter', 'latex', 'fontsize', 17)
+yticks([-pi,-pi/2,0,pi/2,pi])
+yticklabels({'-180','-90','0','90','180'})
+title('Phase', 'fontsize', 20)
