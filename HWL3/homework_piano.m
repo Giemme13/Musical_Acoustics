c37bf8f407b3d16786531e3b82abe689728ba576
Davide <davide.marin@mail.polimi.it>
2021-12-11 19:16:47 +0100

    Some parameters

--- HWL3/homework_piano.m
+++ HWL3/homework_piano.m
@@ -19,14 +19,29 @@ clc
 % as C2.
 
 % Temporal sampling parameters
+length=8; %signal is 8 second long
+T=4*44100; %time resolution
+t=linspace(0,length,length*T);
 
 % Fundamental note
+f=65.4;
 
 % Boundary          
 
 % String parameters
+b1=0.5; %air damping coeff.
+b2=6.25e-9; %internal friction of the string
+%m=;%mass 
+%k=; %stiffness
+%l=; %length
+%T=; %tension
+%u=; %linear density
+c=sqrt(T/u); %speed stiffness, 
 
 % Spatial sampling parameters
+w=0.2; %width of window g
+%X=; %spacial resolution
+
 % Aliasing condition
 % Number of maximum spatial steps
 
@@ -36,8 +51,21 @@ clc
 
 
 % FD parameters
+mu=k^2/(c^2*X^2);
+v=2*b2*T/(X^2);
+lambda=c*T/X;
+a1=(-lambda^2*mu)/(1+b1*T);
+a2=(lambda^2+4*mu*lambda^2+v)/(1+b1*T);
+a3=(2-2*lambda^2-6*mu*lambda^2-2*v)/(1+b1*T);
+a4=(-1+b1*T+2*v)/(1+b1*T);
+a5=(-v)/(1+b1*T);
 
 % Hammer parameters
+Mh=4.9e-3; %mass of the hammer
+bh=1e-4; %fluid damping coeff 
+K=4e8; %stiffness of the hammer
+p=2.3; %stiffness exponent
+V_h0=2.5; %initial velocity of hammer
 
 % Hammer contact window definition
 
@@ -45,9 +73,9 @@ clc
 %PDE Coefficients:
 
 % Bridge boundary coefficients
-
+bound_b=1000;
 % Left hand (hinged string end) boundary coefficients
-
+bound_l=1e20;
 % Hammer felt parameters
 
 %% Computation of the FD scheme
