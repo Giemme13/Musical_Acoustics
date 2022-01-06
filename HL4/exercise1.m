1237b51f1e0464f62091bb802bbf9b70d19fa550
Giemme13 <gianmarco.debortoli@mail.polimi.it>
2022-01-06 16:34:49 +0100

    HL4, ex2, autocorrelation

--- HL4/exercise1.m
+++ HL4/exercise1.m
@@ -15,32 +15,39 @@ clc
 %% Setup
 addpath('Functions');
 
-typeOfSignal = 'sweep/';         % Noise or sweep
+typeOfSignal = 'sweep/';        % Noise or sweep
 
 dir = ['./recordings/' typeOfSignal];  % Recordings directory
 
-nMic = 1;                      % Number of microphones
-signalEnergy =zeros(1,24);     % Vector of energy
+nMic = 1;                       % Number of microphones
+signalEnergy = zeros(1,24);     % Vector of energy
 
 % Labels will be the angle 
-labels=cell(1,24);
-step=15;
-deg=zeros(1,24);
-for i=1:24
+labels = cell(1,24);
+step = 15;                      % one measure every 15 degrees
+deg = zeros(1,24);
+for i = 1:24
    deg(i)=step*(i-1);
-   labels{:,i}=strcat(num2str(deg(i)), '°');
+   labels{:,i} = strcat(num2str(deg(i)), '°');
 end
 
 %% Load the signals and compute the energy
 
-for i=1:24
-   fileName=strcat(dir, num2str(i), '.wav'); %i-th file name
-   [x, Fs]=audioread(fileName); %read i-th audio
-   for j=1:length(x)
-       signalEnergy(i)=signalEnergy(i)+(abs(x(j)))^2; %compute energy for i-th signal
+for i = 1:24
+   fileName = strcat(dir, num2str(i), '.wav');  % i-th file name
+   [x, Fs] = audioread(fileName);               % read i-th audio
+   for j = 1:length(x)
+       signalEnergy(i) = signalEnergy(i)+(abs(x(j)))^2; % compute energy for i-th signal
    end    
 end
 %% Plot the results
 
 figure(1)
-plot(deg, signalEnergy)
+plot(deg, signalEnergy, 'linewidth', 2)
+xlabel('Degrees [°]', 'fontsize', 20)
+title('Energy of the signal', 'fontsize', 20)
+xticks([0,45,90,135,180,225,270,315,360])
+xlim([0,360])
+
+
+
--- HL4/exercise2.m
+++ HL4/exercise2.m
@@ -17,38 +17,55 @@ clc
 %% First reflection estimation using autocorrelation
 % Collecting autocorrelations and plotting them in order to analyze
 % reflections. Try different takes and input signals
+
 addpath('Functions');
 
-nMic = 1;% Number of microphones
-typeOfSignal = 'sweep/';% Noise or sweep
+nMic = 1;                                  % Number of microphones
+typeOfSignal = 'sweep/';                   % Noise or sweep
 dir = ['./recordings/' typeOfSignal];      % Recordings directory
-fileName=strcat(dir, '1.wav');
-speed_of_sound = 343.8; % [m]/[s]
+fileName = strcat(dir, '1.wav');
+speed_of_sound = 343.8;                    % [m]/[s]
 
-%% Plot the signal autocorrelatio
+%% Plot the signal autocorrelation
 
 % Load the signal
-[x, Fs]=audioread(fileName);
+[x, Fs] = audioread(fileName);
 % Time length
-time_length=length(x)/Fs;
+time_length = length(x)/Fs;
 % Time axis
-t=1:length(x);
+t = linspace(0,time_length,length(x))';
 % Auto correlation
-xc=xcorr(x);
-%Half of the autocorrelation
-xc=xc((length(xc)/2):end);
-    
-%% Plot the autocorrelation
-xlimit=length(t)/32;
+[xc, lags] = xcorr(x, 'normalized');
+% Half of the autocorrelation
+xc = xc(round((length(xc)/2)):end);
+lags = lags(round((length(lags)/2)):end);
+
+% Plot 
 figure(1)
+subplot(2,1,1)
+plot(t, x)
+xlabel('Time [s]', 'fontsize', 20)
+title('Original signal', 'fontsize', 20)
+subplot(2,1,2)
 plot(t, xc);
-title(['Mic: ', num2str(n)]);
-axis([0 xlimit -30 50]);    % Limit the axis
-xlabel('Time (sec)');
+xlabel('Time [s]', 'fontsize', 20)
+title('Autocorrelation', 'fontsize', 20)
+sgtitle(['Mic: ', num2str(nMic)], 'fontsize', 20)
+
+
 %% MIC TO REFLECTORS DISTANCE COMPUTATION
-% Put here the difference between first reflection 
-%delay = %[s]
+% Put here the difference between first reflection
+index_reflex = find(abs(t-0.1)==min(abs(t-0.1)));
+xc_reflex = xc(1:index_reflex);
+t_reflex = t(1:index_reflex);
+figure()
+plot(t_reflex,xc_reflex);
+[max,locs] = findpeaks(abs(xc_reflex));
+locs_times = t_reflex(locs);
+figure()
+stem(locs_times,max)
+distances = locs_times*speed_of_sound;
+%delay = 0.06;                        %[s]
+%distance = delay*speed_of_sound;       %[m]
 
-%distance = ;%[m]
-
-%fprintf(sprintf('Average distance between mic and first reflection %f m\n', distance));
+% fprintf(sprintf('Average distance between mic and first reflection %f m\n', distance));
